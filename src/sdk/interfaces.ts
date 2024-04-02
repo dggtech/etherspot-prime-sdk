ccc7a9a079b36141ee450114f173060702bdb162
vukhaihoan <funny.khaihoan@gmail.com>
2024-04-02 10:54:39 +0700

    feat: add Biconomy Token Paymaster

--- src/sdk/interfaces.ts
+++ src/sdk/interfaces.ts
@@ -1,15 +1,23 @@
+import { SponsorUserOperationDto } from '@biconomy/paymaster';
 import { BundlerProviderLike } from './bundler';
 import { StateStorage } from './state';
 
+export enum PaymasterType {
+  BICONOMY = 'biconomy',
+  PIMLICO = 'pimlico',
+}
+
 export interface PaymasterApi {
   url: string;
+  type?: PaymasterType;
   context?: any;
+  paymasterServiceData?: SponsorUserOperationDto;
 }
 
 export enum Factory {
   ZERO_DEV = 'zeroDev',
   ETHERSPOT = 'etherspot',
-  SIMPLE_ACCOUNT = 'simpleAccount'
+  SIMPLE_ACCOUNT = 'simpleAccount',
 }
 
 export interface SdkOptions {
